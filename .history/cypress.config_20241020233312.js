95a805f7224db28a4f193c4aa5b15858827ba3ea
zh <2013471393@qq.com>
2024-10-21 00:16:48 +0800

    Delete .history directory

--- .history/cypress.config_20241020233312.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
-  projectId: "rzyk48",
-  e2e: {
-    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173'
-  }
-})
